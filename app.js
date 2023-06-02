d7abe1f77703b2cd59f03cd564fdc87082f7d92b
rioprayoga23 <yoprayogap@gmail.com>
2023-06-02 15:27:35 +0700

    init project

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors')
+
+const app = express();
+app.use(cors())
+
+// router   
+const categoriesRouter = require('./app/api/v1/categories/router');
+
+const v1 = '/api/v1/cms';
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req,res)=>{
+    res.status(200).json({
+        message:'Welcome to api GoSeminar'
+    })
+});
+
+app.use(v1, categoriesRouter)
+
+module.exports = app;
